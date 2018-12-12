785f84ee6d30533ffd121589327cd38d73f7d950
wesam123hat <wesam_hatem94@hotmail.com>
2018-12-12 10:46:23 +0200

    adding functions

--- sum.js
+++ sum.js
@@ -1,5 +1,13 @@
 
 function sum(a, b) {
-    return a + b +10;
+    return a + b ;
   }
-  module.exports = sum;
+  function multiply(a, b) {
+    return a * b ;
+  }
+  function subtract(a, b) {
+    return a - b ;
+  }
+
+  module.exports = { sum: sum, multiply: multiply ,subtract:subtract }
+  // module.exports = sum;
